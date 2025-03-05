cc2d46e21f7b5e10aa032338538ae5638695998f
11Mysteryfox <Mindbenyamaiklang@gmail.com>
2025-03-05 13:07:51 +0700

    init

---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+export default function Header(){
+    return (
+        <header>
+            <div className="w-full flex items-center justify-between gap-4 p-4 bg-blue-300">
+
+                <div>LOGO</div>
+                <nav>
+                    <ul className="flex gap-4">
+                        <li><Link href={"#"}>Department</Link></li>
+                        <li><Link href={"#"}>Employee</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
